4d0fa78fa0efb5a3c05d3539ec319c02ec5c67ae
Jhonatan Edward <jhonatanedsp@gmail.com>
2021-03-27 13:21:30 -0300

    Multicast route, validation XML, handling errors, using direct etc...

--- camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -1,7 +1,10 @@
 package br.com.caelum.camel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.http4.HttpMethods;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class RotaPedidos {
@@ -9,21 +12,57 @@ public class RotaPedidos {
 	public static void main(String[] args) throws Exception {
 
 		CamelContext context = new DefaultCamelContext();
-		context.addRoutes(new RouteBuilder() {
+		context.addRoutes(new RouteBuilder() {			
 			
 			@Override
 			public void configure() throws Exception {
-				from("file:pedidos?delay=5s&noop=true")
-					.split()
-						.xpath("/pedido/itens/item")
-					.log("${body}")
-					.filter()
-						.xpath("/item/formato[text()='EBOOK']")
-					.log("${id}")
-					.marshal().xmljson()
-					.log("${body}")
-					.setHeader("CamelFileName", simple("${file:name.noext}.json"))
-				.to("file:saida");
+				
+				errorHandler(deadLetterChannel("file:erro").
+						logExhaustedMessageHistory(true)
+						.maximumRedeliveries(3)
+						.redeliveryDelay(2)
+						.onRedelivery(new Processor() {	
+							@Override
+							public void process(Exchange exchange) throws Exception {
+								int counter = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_COUNTER);
+							    int max = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_MAX_COUNTER);
+							    System.out.println("Redelivery - " + counter + "/" + max );
+							}
+						})
+							
+						);
+				
+				from("file:pedidos?delay=5s&noop=true").
+				routeId("rota-pedidos").
+				to("validator:pedido.xsd");
+//				multicast().
+//				 	parallelProcessing().
+//						to("direct:soap").
+//						to("direct:http");
+				
+				from("direct:http").
+				routeId("rota-http").
+				    setProperty("pedidoId", xpath("/pedido/id/text()")).
+				    setProperty("clienteId", xpath("/pedido/pagamento/email-titular/text()")).
+				    split().
+				        xpath("/pedido/itens/item").
+				    filter().
+				        xpath("/item/formato[text()='EBOOK']").
+				    setProperty("ebookId", xpath("/item/livro/codigo/text()")).
+				    // log("${id} \n ${body}").
+				    marshal().
+				        xmljson().
+				    setHeader(Exchange.HTTP_METHOD, HttpMethods.GET).
+				    setHeader(Exchange.HTTP_QUERY, 
+				            simple("clienteId=${property.clienteId}&pedidoId=${property.pedidoId}&ebookId=${property.ebookId}")).
+			    to("http4://localhost:8080/webservices/ebook/item");
+					
+				from("direct:soap").
+					routeId("rota-soap").
+					to("xslt:pedido-para-soap.xslt").
+					log("${body}").
+					setHeader(Exchange.CONTENT_TYPE, constant("text/xml")).
+				to("http4://localhost:8080/webservices/financeiro");
 			}
 		});
 		
