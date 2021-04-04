e3a754f58512d7059195f699c33c1d8fc4987ac1
Jhonatan Edward <jhonatanedsp@gmail.com>
2021-04-04 15:22:35 -0300

    Finalizado curso.

--- camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -1,5 +1,6 @@
 package br.com.caelum.camel;
 
+import org.apache.activemq.camel.component.ActiveMQComponent;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
@@ -12,12 +13,15 @@ public class RotaPedidos {
 	public static void main(String[] args) throws Exception {
 
 		CamelContext context = new DefaultCamelContext();
+		
+		context.addComponent("activemq", ActiveMQComponent.activeMQComponent("tcp://localhost:61616"));
+		
 		context.addRoutes(new RouteBuilder() {			
 			
 			@Override
 			public void configure() throws Exception {
 				
-				errorHandler(deadLetterChannel("file:erro").
+				errorHandler(deadLetterChannel("activemq:queue:pedidos.DLQ").
 						logExhaustedMessageHistory(true)
 						.maximumRedeliveries(3)
 						.redeliveryDelay(2)
@@ -32,13 +36,12 @@ public class RotaPedidos {
 							
 						);
 				
-				from("file:pedidos?delay=5s&noop=true").
+				from("activemq:queue:pedidos").
 				routeId("rota-pedidos").
-				to("validator:pedido.xsd");
-//				multicast().
-//				 	parallelProcessing().
-//						to("direct:soap").
-//						to("direct:http");
+				to("validator:pedido.xsd").
+				multicast().
+						to("direct:soap").
+						to("direct:http");
 				
 				from("direct:http").
 				routeId("rota-http").
